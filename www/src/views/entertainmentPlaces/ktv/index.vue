732f3e3c16490ab9b24842cf9cab5376177f8ed2
Zhang Yusen <zysallen@users.noreply.github.com>
2022-03-21 11:19:45 +0800

    Update index.vue

--- www/src/views/entertainmentPlaces/ktv/index.vue
+++ www/src/views/entertainmentPlaces/ktv/index.vue
@@ -16,7 +16,7 @@
 				</el-form-item>
 			</el-form>
 		</el-header>
-		<div class="ktv-body">
+		<div class="ktv-body"  style="padding: 5px;">
 			<!-- <el-button @click="dialogVisible = true">新增</el-button> -->
 			<div style="height: calc(100vh - 270px)">
 				<el-table :data="tableData" border>
